a733654753c74c568db5139ab8b6771c87374da1
Florian Ehmann <florian.ehmann@gmx.de>
2017-05-08 22:09:38 +0200

    Add some Finesse

--- mousetracker.py
+++ mousetracker.py
@@ -1,8 +1,30 @@
-from pymouse import PyMouse, PyMouseEvent
+from pymouse import PyMouse
+from time import sleep
+import signal
+import sys
 
-m = PyMouse()
+mouse = PyMouse()
+
+capturing = True
+deltat = 0.1
+
+def capture_position():
+    """Loop for mouse capture"""
+    while capturing:
+        sleep(0.1)
+        print("Mouse is at (%d,\t%d)" % mouse.position())
+
+def finish_capture(signal, frame):
+    """Finishes the capture of the movement and saves the data"""
+    global capturing
+    capturing = False
+    print("Finished capture")
 
 if __name__ == "__main__":
     print("Script initialized")
 
-    print("%d, %d" % m.position());
+    # register ctrl c handler
+    signal.signal(signal.SIGINT, finish_capture);
+
+    # go to capture loop
+    capture_position()
