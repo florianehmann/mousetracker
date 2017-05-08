f9888b59e580a994c18ddc23bc965b61b80560bb
Florian Ehmann <florian.ehmann@gmx.de>
2017-05-08 22:38:27 +0200

    Save Mouse Data to File

--- mousetracker.py
+++ mousetracker.py
@@ -2,24 +2,36 @@
 
 from time import sleep
 import signal
+import numpy as np
 from pymouse import PyMouse
 
 MOUSE = PyMouse()
 
 CAPTURING = True
-DELTAT = 0.1
+DELTAT = 1/24
+
+MOUSE_DATA = np.array([], dtype=int)
 
 def capture_position():
     """Loop for mouse capture"""
+    global MOUSE_DATA
     while CAPTURING:
         sleep(DELTAT)
-        print("Mouse is at (%d,\t%d)" % MOUSE.position())
+        mouse_x, mouse_y = MOUSE.position()
+        MOUSE_DATA = np.append(MOUSE_DATA, np.array([mouse_x, mouse_y]))
+        #print("Mouse is at (%d,%d)" % (mouse_x, mouse_y))
 
 def finish_capture(sig, frame):
     """Finishes the capture of the movement and saves the data"""
-    global CAPTURING
+    global CAPTURING, MOUSE_DATA
     CAPTURING = False
     print("Finished capture")
+
+    # save data
+    MOUSE_DATA = np.reshape(MOUSE_DATA, (-1, 2))
+    #print(MOUSE_DATA)
+    headerstring = "Mouse Data\nTime Spacing: %f" % (DELTAT)
+    np.savetxt("mouse_data.txt", MOUSE_DATA, fmt="%d", header=headerstring)
 
 if __name__ == "__main__":
     print("Script initialized")
